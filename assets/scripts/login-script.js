53bad1fa075400974afada4960c7cf4567c02b49
Rendika Tahir Ahmad <76467554+Rendika83@users.noreply.github.com>
2026-08-21 10:52:50 +0700

    Update login-script.js
    adding comments section

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -1,25 +1,29 @@
-/*
- * Catatan:
- * Gunakan fungsi goToHome() untuk menampilkan halaman home
- * Gunakan fungsi goToLogin() untuk menampilkan halaman login
- * Gunakan fungsi showPopUp() untuk menampilkan pop up error
+/**
+ * Membuat variabel loginFormElement untuk tampilan form login.
+ * @constant {HTMLElement}
  */
-
 const loginFormElement = document.querySelector('#loginForm');
+
+/**
+ * Membuat variabel inputEmailElement untuk tampilan input email.
+ * @constant {HTMLElement}
+ */
 const inputEmailElement = document.querySelector('#inputEmail');
+
+/**
+ * Membuat variabel inputPasswordElement untuk tampilan input password.
+ * @constant {HTMLElement}
+ */
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+/**
+ * Membuat variabel expectedEmail sebagai nilai email yang diharapkan.
+ * @constant {string}
+ */
 const expectedEmail = 'admin@dicoding.com';
+
+/**
+ * Membuat variabel expectedPassword sebagai nilai password yang diharapkan.
+ * @constant {string}
+ */
 const expectedPassword = 'superpassword';
-
-loginFormElement.addEventListener('submit', function(event) {
-  event.preventDefault();
-
-  const email = inputEmailElement.value;
-  const password = inputPasswordElement.value;
-  if (email == expectedEmail && password == expectedPassword) {
-    goToHome();
-  } else {
-    showPopUp();
-  }
-});
